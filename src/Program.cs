5e7b40ef4abba0345701754bbefa89c2d549e003
Tim Abell <tim@timwise.co.uk>
2023-12-03 20:58:20 +0000

    Parse the example project

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SlnParser;
+
+namespace sln_items_sync;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        var parser = new SolutionParser();
+        var parsedSolution = parser.Parse(args[0]);
+
+        Console.WriteLine("fin");
+        return 0;
+    }
+}
