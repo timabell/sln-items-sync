242c6b15c4d9436f89d51cddb70273aa7503c2cf
Tim Abell <tim@timwise.co.uk>
2023-12-05 08:14:33 +0000

    Use new SlnEditor nuget to add test solution folders

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
-﻿using SlnParser;
+﻿using SlnEditor;
+using SlnEditor.Contracts;
+using SlnEditor.Helper;
 
 namespace sln_items_sync;
 
@@ -7,7 +9,16 @@ public class Program
     public static int Main(string[] args)
     {
         var parser = new SolutionParser();
-        var parsedSolution = parser.Parse(args[0]);
+        var solutionFilePath = args[0];
+        var solution = parser.Parse(solutionFilePath);
+
+        var solutionFolderTypeGuid = new ProjectTypeMapper().ToGuid(ProjectType.SolutionFolder);
+        var solutionFolder = new SolutionFolder(Guid.NewGuid(), "foo", "foo/", solutionFolderTypeGuid, ProjectType.SolutionFolder);
+        var nestedSolutionFolder = new SolutionFolder(Guid.NewGuid(), "bar", "bar/", solutionFolderTypeGuid, ProjectType.SolutionFolder);
+        solutionFolder.Projects.Add(nestedSolutionFolder);
+        solution.Projects.Add(solutionFolder);
+        var updatedSln = solution.Write();
+        parser.Write(solution, solutionFilePath);
 
         Console.WriteLine("fin");
         return 0;
