67776acb5e61130f58c70bb2f5b806ffaa6f0e70
Tim Abell <tim@timwise.co.uk>
2023-12-11 23:19:00 +0000

    Write expected

--- tests/IntegrationTests.cs
+++ tests/IntegrationTests.cs
@@ -74,6 +74,7 @@ Global
         EndGlobalSection
 EndGlobal
 ";
+        File.WriteAllText(Path.Combine(tmp, "expected.sln"), expected);
 
 
         new CLI(fakeGuidGenerator).Run(new[] { "-s", "target.sln", "rootfile.txt", "subfolder" });
