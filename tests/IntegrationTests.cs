a9ab507e632e005431ec20cb4997978543116fd0
Tim Abell <tim@timwise.co.uk>
2023-12-11 21:02:59 +0000

    updated expected

--- tests/IntegrationTests.cs
+++ tests/IntegrationTests.cs
@@ -14,7 +14,6 @@ public class IntegrationTests(ITestOutputHelper output)
         Directory.CreateDirectory(tmp);
         Directory.SetCurrentDirectory(tmp);
         const string sln = @"
-
 Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio Version 17
 VisualStudioVersion = 17.0.31903.59
@@ -38,21 +37,36 @@ EndGlobal
         File.WriteAllText(Path.Combine(tmp, "subfolder", "nested_folder", "nested_file.txt"), "");
 
         const string expected = @"
-
 Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio Version 17
 VisualStudioVersion = 17.0.31903.59
 MinimumVisualStudioVersion = 10.0.40219.1
+Project(""{2150E333-8FDC-42A3-9474-1A3956D46DE8}"") = ""SolutionItems"", ""SolutionItems"", ""{17591C35-3F90-4F4A-AA13-45CF8D824066}""
+        ProjectSection(SolutionItems) = preProject
+                rootfile.txt = rootfile.txt
+        EndProjectSection
+EndProject
+Project(""{2150E333-8FDC-42A3-9474-1A3956D46DE8}"") = ""subfolder"", ""subfolder/"", ""{CF942CDD-19AC-4E52-9C6E-B1381E0406D9}""
+EndProject
+Project(""{2150E333-8FDC-42A3-9474-1A3956D46DE8}"") = ""nested_folder"", ""nested_folder/"", ""{F5636E74-888A-4FBD-A8E2-9718A05D90BD}""
+        ProjectSection(SolutionItems) = preProject
+                nested_file.txt = nested_file.txt
+        EndProjectSection
+EndProject
 Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|Any CPU = Debug|Any CPU
-		Release|Any CPU = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
+        GlobalSection(SolutionConfigurationPlatforms) = preSolution
+                Debug|Any CPU = Debug|Any CPU
+                Release|Any CPU = Release|Any CPU
+        EndGlobalSection
+        GlobalSection(ProjectConfigurationPlatforms) = postSolution
+        EndGlobalSection
+        GlobalSection(NestedProjects) = preSolution
+                {CF942CDD-19AC-4E52-9C6E-B1381E0406D9} = {17591C35-3F90-4F4A-AA13-45CF8D824066}
+                {F5636E74-888A-4FBD-A8E2-9718A05D90BD} = {CF942CDD-19AC-4E52-9C6E-B1381E0406D9}
+        EndGlobalSection
+        GlobalSection(SolutionProperties) = preSolution
+                HideSolutionNode = FALSE
+        EndGlobalSection
 EndGlobal
 ";
 
