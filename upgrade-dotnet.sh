81c74dedda5ceb2a92b7a5387ec4b746fd5bed17
Tim Abell <t@0x5.uk>
2025-08-06 00:36:37 +0100

    Adjust upgrade-dotnet for asdf v0.18
    Latest asdf has different behaviour:
    - new line on end of version list, so take 2 lines
    - local command gone, replaced with set
    
    Seems to now show up with a blank line on the end of the list

--- upgrade-dotnet.sh
+++ upgrade-dotnet.sh
@@ -2,9 +2,9 @@
 set -e # exit on error
 asdf plugin update dotnet-core
 # filter out -preview releases, and assume last one is newest
-latest=`asdf list all dotnet-core | grep -v "-" | tail -n 1`
+latest=`asdf list all dotnet-core | grep -v "-" | tail -n 2`
 echo $latest
 asdf install dotnet-core $latest
-asdf local dotnet-core $latest
+asdf set dotnet-core $latest
 dotnet test
 git commit --include .tool-versions --message "Upgrade dotnet-core to latest"
